211b4b18cb8ff933ae3b465096ab78b1eb29eb4d
Edmore Moyo <etmoyo@gmail.com>
2025-09-16 14:17:19 -0400

    reads from input directory

--- agent.sh
+++ agent.sh
@@ -4,10 +4,10 @@ pennsieve agent
 pennsieve whoami
 pennsieve dataset use $1
 
-ls -alh /mnt/efs/output/$2
+ls -alh $3
 timestamp=$(date +%Y%m%d_%H%M%S%Z)
 target_path="${TARGET_PATH:-"output-$timestamp-$2"}"
 
-pennsieve manifest create /mnt/efs/output/$2 -t $target_path
+pennsieve manifest create $3 -t $target_path
 pennsieve manifest list 1
 pennsieve upload manifest 1
